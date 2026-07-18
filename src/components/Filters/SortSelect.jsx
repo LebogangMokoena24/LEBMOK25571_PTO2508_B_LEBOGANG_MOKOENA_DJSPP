1476e4cebf59248773248b45d0a7d94594ec4d72
Lebogang Mokoena <ditswe24@gmail.com>
2026-07-18 22:31:21 +0200

    Add SortSelect component for sorting options

--- src/components/Filters/SortSelect.jsx
+++ src/components/Filters/SortSelect.jsx
@@ -1,1 +1,21 @@
+import { useContext } from "react";
+import { SORT_OPTIONS, PodcastContext } from "../../context/PodcastContext";
+import styles from "./SortSelect.module.css";
 
+export default function SortSelect() {
+  const { sortKey, setSortKey } = useContext(PodcastContext);
+
+  return (
+    <select
+      className={styles.select}
+      value={sortKey}
+      onChange={(e) => setSortKey(e.target.value)}
+    >
+      {SORT_OPTIONS.map((o) => (
+        <option key={o.key} value={o.key}>
+          {o.label}
+        </option>
+      ))}
+    </select>
+  );
+}
