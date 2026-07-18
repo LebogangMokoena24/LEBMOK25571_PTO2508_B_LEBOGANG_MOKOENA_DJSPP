ede809a308ecae9c635e2b8ec07cf5295b4e3b02
Lebogang Mokoena <ditswe24@gmail.com>
2026-07-18 22:33:13 +0200

    Implement GenreFilter component with context

--- src/components/Filters/GenreFilter.jsx
+++ src/components/Filters/GenreFilter.jsx
@@ -1,1 +1,22 @@
+import { useContext } from "react";
+import { PodcastContext } from "../../context/PodcastContext";
+import styles from "./GenreFilter.module.css";
 
+export default function GenreFilter({ genres }) {
+  const { genre, setGenre } = useContext(PodcastContext);
+
+  return (
+    <select
+      className={styles.select}
+      value={genre}
+      onChange={(e) => setGenre(e.target.value)}
+    >
+      <option value="all">All Genres</option>
+      {genres.map((g) => (
+        <option key={g.id} value={g.id}>
+          {g.title}
+        </option>
+      ))}
+    </select>
+  );
+}
