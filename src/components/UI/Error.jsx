f06155da9bd5fad28f8fabb6e686fbdb5701bebc
Lebogang Mokoena <ditswe24@gmail.com>
2026-07-19 11:05:28 +0200

    Add Error component with styling

--- src/components/UI/Error.jsx
+++ src/components/UI/Error.jsx
@@ -1,1 +1,9 @@
+import styles from "./Error.module.css";
 
+export default function Error({ message }) {
+  return (
+    <div className={styles.messageContainer}>
+      <div className={styles.error}>{message}</div>
+    </div>
+  );
+}
