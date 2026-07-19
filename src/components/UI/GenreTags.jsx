f4a92e4494eb8e3c76b720cbcab99b179b8c2085
Lebogang Mokoena <ditswe24@gmail.com>
2026-07-19 11:06:25 +0200

    Add GenreTags component to display genre titles

--- src/components/UI/GenreTags.jsx
+++ src/components/UI/GenreTags.jsx
@@ -1,1 +1,14 @@
+import { genres as genreMap } from "../../data";
+import styles from "./GenreTags.module.css";
 
+export default function GenreTags({ genres }) {
+  const genreSpans = genres.map((id) => {
+    const match = genreMap.find((genre) => genre.id === id);
+    return (
+      <span key={id} className={styles.tag}>
+        {match ? match.title : `Unknown (${id})`}
+      </span>
+    );
+  });
+  return <div className={styles.tags}>{genreSpans}</div>;
+}
