59164c4fd850e3af6f823898424878655c68fbca
Lebogang Mokoena <ditswe24@gmail.com>
2026-07-19 11:27:18 +0200

    Set up routing and context providers in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,33 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/UI/Header";
+import Home from "./pages/Home";
+import ShowDetail from "./pages/ShowDetail";
+import Favourites from "./pages/Favourites";
+import { PodcastProvider } from "./context/PodcastContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import { FavoritesProvider } from "./context/FavoritesContext";
+import { ListeningProgressProvider } from "./context/ListeningProgressContext";
+import { AudioPlayerProvider } from "./context/AudioPlayerContext";
+import { GlobalAudioPlayer } from "./components/Player";
 
+export default function App() {
+  return (
+    <ThemeProvider>
+      <FavoritesProvider>
+        <ListeningProgressProvider>
+          <AudioPlayerProvider>
+            <Header />
+            <PodcastProvider>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path={`/show/:id`} element={<ShowDetail />} />
+                <Route path="/favourites" element={<Favourites />} />
+              </Routes>
+            </PodcastProvider>
+            <GlobalAudioPlayer />
+          </AudioPlayerProvider>
+        </ListeningProgressProvider>
+      </FavoritesProvider>
+    </ThemeProvider>
+  );
+}
