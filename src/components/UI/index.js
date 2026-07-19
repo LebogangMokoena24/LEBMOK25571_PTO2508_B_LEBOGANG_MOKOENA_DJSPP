389c1326ca38c400c19c986af64e7917e65f4bfe
Lebogang Mokoena <ditswe24@gmail.com>
2026-07-19 11:33:57 +0200

    Export multiple UI components from index.js

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,1 +1,6 @@
-
+export { default as Pagination } from "./Pagination";
+export { default as Loading } from "./Loading";
+export { default as Error } from "./Error";
+export { default as GenreTags } from "./GenreTags";
+export { default as Header } from "./Header";
+export { default as ThemeToggle } from "./ThemeToggle";
