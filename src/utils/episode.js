21db65c81965d474b814b7c98a88edd6c778a197
Lebogang Mokoena <ditswe24@gmail.com>
2026-07-19 11:14:54 +0200

    Add placeholder audio URL and getEpisodeId function

--- src/utils/episode.js
+++ src/utils/episode.js
@@ -1,1 +1,6 @@
+export const PLACEHOLDER_AUDIO_URL =
+  "https://podcast-api.netlify.app/placeholder-audio.mp3";
 
+export function getEpisodeId(showId, seasonIndex, episodeIndex) {
+  return `${showId}-${seasonIndex}-${episodeIndex}`;
+}
